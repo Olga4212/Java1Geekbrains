c815347e6eb4c4c44ed006aab301e37c2b29e79d
Olga <fenex@bk.ru>
2020-05-01 15:33:38 +0300

    lesson3 task *

--- src/lesson3/TicTacToe.java
+++ src/lesson3/TicTacToe.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private static int fieldSizeY;
-    private static int fieldSizeX;
     private static char[][] field;
 
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -16,13 +14,15 @@ public class TicTacToe {
     private static final char AI_DOT = 'O';
     private static final char EMPTY_DOT = '.';
 
+    private static final int FIELD_SIZE_X = 5;
+    private static final int FIELD_SIZE_Y = 6;
+    private static final int WIN_LINE_LENGTH = 3;
+
     // init field
     private static void initMap() {
-        fieldSizeY = 3;
-        fieldSizeX = 3;
-        field = new char[fieldSizeY][fieldSizeX];
-        for (int y = 0; y < fieldSizeY; y++) {
-            for (int x = 0; x < fieldSizeX; x++) {
+        field = new char[FIELD_SIZE_Y][FIELD_SIZE_X];
+        for (int y = 0; y < FIELD_SIZE_Y; y++) {
+            for (int x = 0; x < FIELD_SIZE_X; x++) {
                 field[y][x] = EMPTY_DOT;
             }
         }
@@ -31,9 +31,9 @@ public class TicTacToe {
     // print field
     private static void printMap() {
         System.out.println("-------");
-        for (int y = 0; y < fieldSizeY; y++) {
+        for (int y = 0; y < FIELD_SIZE_Y; y++) {
             System.out.print("|");
-            for (int x = 0; x < fieldSizeX; x++) {
+            for (int x = 0; x < FIELD_SIZE_X; x++) {
                 System.out.print(field[y][x] + "|");
             }
             System.out.println();
@@ -45,7 +45,7 @@ public class TicTacToe {
         int x;
         int y;
         do {
-            System.out.printf("Введите координаты хода X и Y (от 1 до %d) через пробел: ", fieldSizeX);
+            System.out.printf("Введите координаты хода X (от 1 до %d) и Y (от 1 до %d) через пробел: ", FIELD_SIZE_X, FIELD_SIZE_Y);
             x = SCANNER.nextInt() - 1;
             y = SCANNER.nextInt() - 1;
         } while (!isValidCell(y, x) || !isEmptyCell(y, x));
@@ -59,7 +59,7 @@ public class TicTacToe {
 
     // is cell valid
     private static boolean isValidCell(int y, int x) {
-        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+        return x >= 0 && x < FIELD_SIZE_X && y >= 0 && y < FIELD_SIZE_Y;
     }
 
     // ai turn
@@ -67,64 +67,111 @@ public class TicTacToe {
         int x;
         int y;
         do {
-            x = RANDOM.nextInt(fieldSizeX);
-            y = RANDOM.nextInt(fieldSizeY);
+            x = RANDOM.nextInt(FIELD_SIZE_X);
+            y = RANDOM.nextInt(FIELD_SIZE_Y);
         } while (!isEmptyCell(y, x));
         field[y][x] = AI_DOT;
     }
 
     // check win
-    private static boolean checkWin(char c) {
-        for (int y = 0; y < fieldSizeY; y++){
-            boolean isRowWin = true;
-            for (int x = 0; x < fieldSizeX; x++) {
-                if (field[y][x] != c ) {
-                    isRowWin = false;
+    private static boolean checkWin(char testedSymbol) {
+        int maxFieldSize = Math.max(FIELD_SIZE_X, FIELD_SIZE_Y);
+
+        // check rows and cols
+        for (int a = 0; a < maxFieldSize; a++) {
+            int rowSum = 0;
+            int columnSum = 0;
+            for (int b = 0; b < maxFieldSize; b++) {
+
+                if (a < FIELD_SIZE_Y && b < FIELD_SIZE_X) {
+                    if (field[a][b] == testedSymbol) {
+                        rowSum++;
+                        if (rowSum >= WIN_LINE_LENGTH) {
+                            return true;
+                        }
+                    } else {
+                        rowSum = 0;
+                    }
+                }
+
+                if (a < FIELD_SIZE_X && b < FIELD_SIZE_Y) {
+                    if (field[b][a] == testedSymbol) {
+                        columnSum++;
+                        if (columnSum >= WIN_LINE_LENGTH) {
+                            return true;
+                        }
+                    } else {
+                        columnSum = 0;
+                    }
                 }
             }
-            if (isRowWin){
-                return true;
-            }
         }
 
-        for (int x = 0; x < fieldSizeX; x++){
-            boolean isColWin = true;
-            for (int y = 0; y < fieldSizeY; y++) {
-                if (field[y][x] != c ) {
-                    isColWin = false;
+        // check diagonals 1
+        for (int x = 0; x < maxFieldSize; x++) {
+            int diagonal1Sum = 0;
+            int diagonal2Sum = 0;
+            for (int y = 0; y < maxFieldSize; y++) {
+                if (y < FIELD_SIZE_Y && y + x < FIELD_SIZE_X) {
+                    if (field[y][y + x] == testedSymbol) {
+                        diagonal1Sum++;
+                        if (diagonal1Sum >= WIN_LINE_LENGTH) {
+                            return true;
+                        }
+                    } else {
+                        diagonal1Sum = 0;
+                    }
+                }
+
+                if (y < FIELD_SIZE_Y && y - x >= 0 && y - x < FIELD_SIZE_X) {
+                    if (field[y][y - x] == testedSymbol) {
+                        diagonal2Sum++;
+                        if (diagonal2Sum >= WIN_LINE_LENGTH) {
+                            return true;
+                        }
+                    } else {
+                        diagonal2Sum = 0;
+                    }
                 }
             }
-            if (isColWin){
-                return true;
+        }
+
+        // check diagonals 2
+        for (int x = 0; x < maxFieldSize; x++) {
+            int diagonal1Sum = 0;
+            int diagonal2Sum = 0;
+            for (int y = 0; y < maxFieldSize; y++) {
+                if (y + x < FIELD_SIZE_Y && FIELD_SIZE_X - y - 1 >= 0) {
+                    if (field[y + x][FIELD_SIZE_X - y - 1] == testedSymbol) {
+                        diagonal1Sum++;
+                        if (diagonal1Sum >= WIN_LINE_LENGTH) {
+                            return true;
+                        }
+                    } else {
+                        diagonal1Sum = 0;
+                    }
+                }
+
+                if (y - x >= 0 && y - x < FIELD_SIZE_X && FIELD_SIZE_X - y - 1 >= 0) {
+                    if (field[y - x][FIELD_SIZE_X - y - 1] == testedSymbol) {
+                        diagonal2Sum++;
+                        if (diagonal2Sum >= WIN_LINE_LENGTH) {
+                            return true;
+                        }
+                    } else {
+                        diagonal2Sum = 0;
+                    }
+                }
             }
         }
 
-        boolean isDiagonal1Win = true;
-        for (int x = 0; x < fieldSizeX; x++){
-            if (field[x][x] != c) {
-                isDiagonal1Win = false;
-            }
-        }
-        if (isDiagonal1Win) {
-            return true;
-        }
-
-        boolean isDiagonal2Win = true;
-        for (int x = 0; x < fieldSizeX; x++){
-            if (field[x][fieldSizeX-x-1] != c) {
-                isDiagonal2Win = false;
-            }
-        }
-        if (isDiagonal2Win) {
-            return true;
-        }
         return false;
     }
 
     // check draw
     private static boolean isMapFull() {
-        for (int y = 0; y < fieldSizeY; y++) {
-            for (int x = 0; x < fieldSizeX; x++) {
+        for (int y = 0; y < FIELD_SIZE_Y; y++) {
+            for (int x = 0; x < FIELD_SIZE_X; x++) {
                 if (isEmptyCell(y, x)) return false;
             }
         }
@@ -148,11 +195,10 @@ public class TicTacToe {
                 break;
         }
         SCANNER.close();
-
     }
 
-    private static boolean isGameFinished(char aiDot, String s) {
-        if (checkWin(aiDot)) {
+    private static boolean isGameFinished(char checkIsWinner, String s) {
+        if (checkWin(checkIsWinner)) {
             System.out.println(s);
             return true;
         }
